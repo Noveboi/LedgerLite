af4982d77d6216d2e2da6655c98409eac24f7903
George Nikolaidis <cptgiorgos03@gmail.com>
2025-04-19 17:15:19 +0300

    Add FastEndpoints and create a testing PingEndpoint

---
+++ LedgerLite/LedgerLite.WebApi/PingEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+
+namespace LedgerLite.WebApi;
+
+public sealed record PingResponse(string Message, string Status);
+public sealed class PingEndpoint : EndpointWithoutRequest<PingResponse>
+{
+    public override void Configure()
+    {
+        Get("/");
+        AllowAnonymous();
+        
+        Summary(endpoint =>
+        {
+            endpoint.Summary = "Used to ping the LedgerLite API for testing or availability";
+        });
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await SendAsync(
+            response: new PingResponse("Hello!", "Healthy"), 
+            cancellation: ct);
+    }
+}
