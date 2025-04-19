493e8220620aba0e6379d145c37b4ac040dd1fa9
George Nikolaidis <cptgiorgos03@gmail.com>
2025-04-19 19:45:54 +0300

    Create entity business logic

--- LedgerLite/src/LedgerLite.SharedKernel/Entity.cs
+++ LedgerLite/src/LedgerLite.SharedKernel/Entity.cs
@@ -2,10 +2,22 @@
 
 public abstract class Entity
 {
-    private Entity() { }
+    protected Entity() { }
     protected Entity(Guid? id)
     {
+        Id = (id == Guid.Empty ? GenerateId() : id) ?? GenerateId();
     }
 
     public Guid Id { get; private init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Entity entity)
+            return Id == entity.Id;
+
+        return false;
+    }
+
+    public override int GetHashCode() => Id.GetHashCode();
+    private static Guid GenerateId() => Guid.CreateVersion7();
 }
