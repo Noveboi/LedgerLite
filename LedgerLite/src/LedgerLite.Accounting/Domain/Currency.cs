21bda850a500c8fa69dac609b53d541ad5b8a1f5
George Nikolaidis <cptgiorgos03@gmail.com>
2025-04-19 21:22:26 +0300

    Add Currency enumeration

---
+++ LedgerLite/src/LedgerLite.Accounting/Domain/Currency.cs
@@ -0,0 +1,10 @@
+﻿using Ardalis.SmartEnum;
+
+namespace LedgerLite.Accounting.Domain;
+
+public sealed class Currency(string name, int value) : SmartEnum<Currency>(name, value)
+{
+    public static readonly Currency Euro = new("EUR", 1);
+    public static readonly Currency Dollar = new("USD", 2);
+    public static readonly Currency BritishPound = new("GBP", 3);
+}
