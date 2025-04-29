758331bd03148a645e1d91b74e994535ea3f59de
George Nikolaidis <nove@novesoftware.net>
2025-04-29 17:05:39 +0300

    DOC: Correct explanations for debit and credit

--- LedgerLite/src/LedgerLite.Accounting/Domain/TransactionType.cs
+++ LedgerLite/src/LedgerLite.Accounting/Domain/TransactionType.cs
@@ -3,11 +3,11 @@
 public enum TransactionType
 {
     /// <summary>
-    /// Money flowing out, credit increases the value of liability, equity and revenue.
+    /// Increases the value of liability, equity and revenue.
     /// </summary>
     Credit = 1, 
     /// <summary>
-    /// Money flowing in, debit increases the value of assets and expenses.
+    /// Debit increases the value of assets and expenses.
     /// </summary>
     Debit = 2
 }
