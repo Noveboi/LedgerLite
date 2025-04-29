11b39f6fd0aa81095dfed803ebad9f8effc2ca15
George Nikolaidis <nove@novesoftware.net>
2025-04-29 17:04:55 +0300

    RENAME: JournalEntryRow -> JournalEntryLine

--- LedgerLite/src/LedgerLite.Accounting/Domain/JournalEntryLine.cs
+++ LedgerLite/src/LedgerLite.Accounting/Domain/JournalEntryLine.cs
@@ -7,18 +7,23 @@ namespace LedgerLite.Accounting.Domain;
 /// <summary>
 /// Records a financial transaction.
 /// </summary>
-public sealed class JournalEntryRow : Entity
+public sealed class JournalEntryLine : Entity
 {
-    private JournalEntryRow() { }
+    private JournalEntryLine() { }
     
+    /// <summary>
+    /// The ID of the associated journal entry.
+    /// </summary>
     public Guid EntryId { get; private init; }
+    /// <summary>
+    /// The ID of the associated account.
+    /// </summary>
     public Guid AccountId { get; private init; }
     public TransactionType TransactionType { get; private init; }
     public DateTime OccuredAtUtc { get; private init; }
-    public Money Money { get; private init; }
+    public Money Money { get; private init; } = null!;
     
-
-    public static JournalEntryRow Create(
+    public static JournalEntryLine Create(
         TransactionType type, 
         Money money, 
         Guid accountId, 
