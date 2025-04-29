f9f61ff6bd3ccefcececf132648dd747969ed25e
George Nikolaidis <nove@novesoftware.net>
2025-04-29 18:39:36 +0300

    ADD PROJ: Add PrivateFaker in new LedgerLite.Tests.Shared project

---
+++ LedgerLite/test/LedgerLite.Tests.Shared/PrivateFaker.cs
@@ -0,0 +1,10 @@
+﻿using Bogus;
+
+namespace LedgerLite.Tests.Shared;
+
+public sealed class PrivateFaker<T> : Faker<T> where T : class 
+{
+    public PrivateFaker<T> UsePrivateConstructor() =>
+        (base.CustomInstantiator(_ => (Activator.CreateInstance(typeof(T), nonPublic: true) as T)! )
+            as PrivateFaker<T>)!;
+}
