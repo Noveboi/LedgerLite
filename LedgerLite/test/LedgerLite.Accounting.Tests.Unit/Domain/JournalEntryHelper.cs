7edfda873e888c3360c7031380009297e6a0d771
George Nikolaidis <nove@novesoftware.net>
2025-04-29 18:40:31 +0300

    Add tests for JournalEntry.Record()

---
+++ LedgerLite/test/LedgerLite.Accounting.Tests.Unit/Domain/JournalEntryHelper.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Domain.JournalEntries;
+
+namespace LedgerLite.Accounting.Tests.Unit.Domain;
+
+/// <summary>
+/// Utility methods for testing journal entries.
+/// </summary>
+internal static class JournalEntryHelper
+{
+    public static Result<JournalEntry> Create(
+        JournalEntryType type,
+        IEnumerable<JournalEntryLine> lines)
+    {
+        return JournalEntry.Record(
+            accountId: Guid.NewGuid(),
+            type: type,
+            referenceNumber: "",
+            description: "",
+            occuredAtUtc: DateTime.Today,
+            lines: lines);
+    }  
+}
---
+++ LedgerLite/test/LedgerLite.Accounting.Tests.Unit/Domain/JournalEntryTypes.cs
@@ -0,0 +1,11 @@
+﻿using LedgerLite.Accounting.Domain.JournalEntries;
+
+namespace LedgerLite.Accounting.Tests.Unit.Domain;
+
+public static class JournalEntryTypes
+{
+    public static TheoryData<JournalEntryType> AllTypesExceptCompound => new(
+        JournalEntryType.List.Except([JournalEntryType.Compound]));
+    
+    public static TheoryData<JournalEntryType> AllTypes => new(JournalEntryType.List);
+}
