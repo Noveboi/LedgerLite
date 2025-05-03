95920d140f5600d5af62d80652c774a43748b3bf
George Nikolaidis <nove@novesoftware.net>
2025-05-03 10:23:37 +0300

    ➕ Create journal entry line request

---
+++ LedgerLite/src/LedgerLite.Accounting/Application/CreateJournalEntryLineRequest.cs
@@ -0,0 +1,7 @@
+﻿using LedgerLite.Accounting.Domain;
+
+namespace LedgerLite.Accounting.Application;
+
+public sealed record CreateJournalEntryLineRequest(
+    Guid AccountId,
+    decimal Amount);
