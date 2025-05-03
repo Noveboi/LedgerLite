5fbd45cf63bcfbf0f79fcd6646b0cebffaf5a56f
George Nikolaidis <nove@novesoftware.net>
2025-05-03 11:21:46 +0300

    Add 'Accounting' module DbContext

---
+++ LedgerLite/src/LedgerLite.Accounting.Core/Infrastructure/AccountingDbContext.cs
@@ -0,0 +1,23 @@
+﻿using LedgerLite.Accounting.Core.Domain;
+using LedgerLite.Accounting.Core.Domain.Accounts;
+using LedgerLite.Accounting.Core.Domain.JournalEntries;
+using LedgerLite.Accounting.Core.Infrastructure.Configuration;
+using LedgerLite.SharedKernel.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace LedgerLite.Accounting.Core.Infrastructure;
+
+internal sealed class AccountingDbContext(DbContextOptions<AccountingDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .ConfigureEnumeration<AccountType>()
+            .ConfigureEnumeration<JournalEntryType>()
+            .ConfigureEnumeration<JournalEntryStatus>()
+            .ConfigureEnumeration<TransactionType>()
+            .ConfigureEnumeration<Currency>();
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IAccountingEntityConfigurationMarker).Assembly);
+    }
+}
