cbf15ebbb59a794a23d941a865848d11f8228dac
George Nikolaidis <nove@novesoftware.net>
2025-05-03 11:20:39 +0300

    ➕ Add EF Core entity configuration extensions

---
+++ LedgerLite/src/LedgerLite.SharedKernel/Persistence/ConfigurationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq.Expressions;
+using Ardalis.SmartEnum;
+using LedgerLite.SharedKernel.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LedgerLite.SharedKernel.Persistence;
+
+public static class ConfigurationExtensions
+{
+    public static void IsDomainEntity<T>(this EntityTypeBuilder<T> builder) where T : Entity
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever();
+    }
+
+    public static void HasEnumeration<T, TProp>(
+        this EntityTypeBuilder<T> builder,
+        Expression<Func<T, TProp?>> propertyExpression) 
+        where TProp : SmartEnum<TProp> 
+        where T : class
+    {
+        builder.HasOne(propertyExpression).WithMany();
+        builder.Property(propertyExpression)
+            .HasConversion(
+                convertToProviderExpression: prop => prop!.Value,
+                convertFromProviderExpression: value => SmartEnum<TProp>.FromValue(value));
+    }
+
+    public static ModelBuilder ConfigureEnumeration<TEnum>(this ModelBuilder builder) where TEnum : SmartEnum<TEnum>
+    {
+        var enumBuilder = builder.Entity<TEnum>();
+        
+        enumBuilder.HasKey(x => x.Value);
+        enumBuilder.HasData(SmartEnum<TEnum>.List);
+        
+        return builder;
+    }
+}
