6c715fb87f93a182feff45620c4ab0dbd152b90d
George Nikolaidis <nove@novesoftware.net>
2025-05-03 10:23:21 +0300

    ➕ Transaction recording service with 'RecordStandardEntry' use case and request

---
+++ LedgerLite/src/LedgerLite.Accounting/Application/ITransactionRecordingService.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Domain.JournalEntries;
+
+namespace LedgerLite.Accounting.Application;
+
+/// <summary>
+/// Orchestrates use cases involving the recording of financial transactions
+/// </summary>
+public interface ITransactionRecordingService
+{
+    Task<Result<JournalEntry>> RecordStandardEntryAsync(RecordStandardEntryRequest req, CancellationToken ct);
+}
---
+++ LedgerLite/src/LedgerLite.Accounting/Application/RecordStandardEntryRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace LedgerLite.Accounting.Application;
+
+public sealed record RecordStandardEntryRequest(
+    string ReferenceNumber,
+    DateTime OccursAtUtc, 
+    string Description,
+    CreateJournalEntryLineRequest CreditLine,
+    CreateJournalEntryLineRequest DebitLine);
