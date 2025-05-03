b0299bfb7506f05e4fb3320bf4f52f2deaca34ae
George Nikolaidis <nove@novesoftware.net>
2025-05-03 11:22:34 +0300

    Add PostgresSQL adapter for 'Accounting' module

--- LedgerLite/src/LedgerLite.Accounting.Core/DependencyInjection.cs
+++ LedgerLite/src/LedgerLite.Accounting.Core/DependencyInjection.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using LedgerLite.Accounting.Core.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LedgerLite.Accounting.Core;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddAccountingModule(this IServiceCollection services)
+    public static IServiceCollection AddAccountingModule(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<AccountingDbContext>(options => options
+            .UseNpgsql(configuration.GetConnectionString("AccountingDatabase")));
+
         return services;
     }
 }
