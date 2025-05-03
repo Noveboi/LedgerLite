6e7a3429c996082135258bda883185ea2be85734
George Nikolaidis <nove@novesoftware.net>
2025-05-03 08:54:32 +0300

    Add 'Accounting' unit of work interface

---
+++ LedgerLite/src/LedgerLite.Accounting/Infrastructure/Persistence/IAccountingUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using LedgerLite.SharedKernel.Persistence;
+
+namespace LedgerLite.Accounting.Infrastructure.Persistence;
+
+public interface IAccountingUnitOfWork : IUnitOfWork
+{
+    IChartOfAccountsRepository ChartOfAccountsRepository { get; }
+    IJournalEntryRepository JournalEntryRepository { get; }
+    IJournalEntryLineRepository JournalEntryLineRepository { get; }
+}
