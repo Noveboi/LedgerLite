752e0a4d0aa403404089e75775068b06182c3c99
George Nikolaidis <nove@novesoftware.net>
2025-05-04 15:25:43 +0300

    Add `IAccountService`

---
+++ LedgerLite/src/LedgerLite.Accounting.Core/Application/IAccountService.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Core.Domain;
+using LedgerLite.Accounting.Core.Domain.Accounts;
+
+namespace LedgerLite.Accounting.Core.Application;
+
+public interface IAccountService
+{
+    Task<Result<Account>> CreateAccountAsync(CreateAccountRequest request, CancellationToken token);
+    Task<Result<Account>> RemoveAccountAsync(CancellationToken token);
+}
+
+public sealed record CreateAccountRequest(
+    string Name,
+    string Number,
+    AccountType Type,
+    Currency Currency,
+    bool IsPlaceholder,
+    string Description);
