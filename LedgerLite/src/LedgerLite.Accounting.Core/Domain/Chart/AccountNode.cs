1f1019d71a433b11a47774a3dc4436b394e2374e
George Nikolaidis <nove@novesoftware.net>
2025-05-04 20:22:48 +0300

    🔄️ REFACTOR: Add children properties to `AccountNode` and change methods for better readability and functionality

--- LedgerLite/src/LedgerLite.Accounting.Core/Domain/Chart/AccountNode.cs
+++ LedgerLite/src/LedgerLite.Accounting.Core/Domain/Chart/AccountNode.cs
@@ -1,4 +1,5 @@
-﻿using LedgerLite.Accounting.Core.Domain.Accounts;
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Core.Domain.Accounts;
 using LedgerLite.SharedKernel.Domain;
 
 namespace LedgerLite.Accounting.Core.Domain.Chart;
@@ -11,21 +12,58 @@ public sealed class AccountNode : Entity
         ChartId = chartId;
     }
     
+    /// <summary>
+    /// The <see cref="ChartOfAccounts"/> which contains this node.
+    /// </summary>
     public Guid ChartId { get; private init; }
+    
+    /// <summary>
+    /// The underlying account that is wrapped by the node.
+    /// </summary>
     public Account Account { get; private set; } = null!;
     
     public Guid? ParentId { get; private set; }
-    public Account? Parent { get; private set; }
+    public AccountNode? Parent { get; private set; }
 
-    public static AccountNode CreateRoot(Guid chartId, Account account) => new(chartId)
+    private readonly List<AccountNode> _children = [];
+    public IReadOnlyCollection<AccountNode> Children => _children;
+    
+    public static AccountNode Create(Guid chartId, Account account) => new(chartId)
     {
         Account = account
     };
 
-    public static AccountNode CreateWithParent(Guid chartId, Account account, Account parent) => new(chartId)
+    private static AccountNode CreateAttachedTo(AccountNode parent, Account account, Guid chartId)
     {
-        Account = account,
-        Parent = parent,
-        ParentId = parent.Id
-    };
+        var node = new AccountNode(chartId)
+        {
+            Account = account,
+            Parent = parent,
+            ParentId = parent.Id
+        };
+
+        return node;
+    }
+
+    public Result<AccountNode> AddChild(Account child)
+    {
+        if (Account == child)
+            return Result.Invalid(AccountErrors.AddAccountToItself());
+
+        if (!Account.IsPlaceholder)
+            return Result.Invalid(AccountErrors.NoChildrenWhenNotPlaceholder());
+
+        if (Account.Type != child.Type)
+            return Result.Invalid(AccountErrors.ChildHasDifferentType(
+                expected: Account.Type, 
+                actual: child.Type));
+        
+        if (_children.Any(node => node.Account == child))
+            return Result.Invalid(ChartOfAccountsErrors.AccountAlreadyExists(child));
+
+        var childNode = CreateAttachedTo(this, child, ChartId);
+        _children.Add(childNode);
+
+        return childNode;
+    }
 }
