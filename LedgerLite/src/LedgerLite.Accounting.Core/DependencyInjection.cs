1d09a6abcc2de540ae34d0a8f1808ce24a3a9d8c
George Nikolaidis <nove@novesoftware.net>
2025-05-05 13:21:35 +0300

    Update general DI for Accounting.Core

--- LedgerLite/src/LedgerLite.Accounting.Core/DependencyInjection.cs
+++ LedgerLite/src/LedgerLite.Accounting.Core/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using LedgerLite.Accounting.Core.Infrastructure;
+﻿using LedgerLite.Accounting.Core.Application;
+using LedgerLite.Accounting.Core.Infrastructure;
+using LedgerLite.SharedKernel.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,7 +12,11 @@ public static class DependencyInjection
     public static IServiceCollection AddAccountingModule(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AccountingDbContext>(options => options
-            .UseNpgsql(configuration.GetConnectionString("AccountingDatabase")));
+            .UseNpgsql(configuration.GetConnectionString("AccountingDatabase"))
+            .AddAuditLogging());
+
+        services.AddAccountingInfrastructure();
+        services.AddAccountingApplication();
 
         return services;
     }
