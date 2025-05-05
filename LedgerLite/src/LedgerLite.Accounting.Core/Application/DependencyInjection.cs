b4eba1d0deb07ecb8cabdfad800b90776125df55
George Nikolaidis <nove@novesoftware.net>
2025-05-05 13:16:29 +0300

    Add separate DI file for Accounting.Core application layer

---
+++ LedgerLite/src/LedgerLite.Accounting.Core/Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LedgerLite.Accounting.Core.Application;
+
+internal static class DependencyInjection
+{
+    public static IServiceCollection AddAccountingApplication(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IAccountService, AccountService>()
+            .AddScoped<ITransactionRecordingService, TransactionRecordingService>();
+    }
+}
