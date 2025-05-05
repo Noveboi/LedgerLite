3d7fe4edbf72a2d0601b7eede576e38282c620e4
George Nikolaidis <nove@novesoftware.net>
2025-05-05 13:14:45 +0300

    Add AccountNodeConfiguration

---
+++ LedgerLite/src/LedgerLite.Accounting.Core/Infrastructure/Configuration/AccountNodeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using LedgerLite.Accounting.Core.Domain.Chart;
+using LedgerLite.SharedKernel.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LedgerLite.Accounting.Core.Infrastructure.Configuration;
+
+internal sealed class AccountNodeConfiguration : IEntityTypeConfiguration<AccountNode>
+{
+    public void Configure(EntityTypeBuilder<AccountNode> builder)
+    {
+        builder.IsDomainEntity();
+        
+        builder.Navigation(x => x.Parent).IsRequired(false);
+        builder.Navigation(x => x.Account).AutoInclude();
+    }
+}
