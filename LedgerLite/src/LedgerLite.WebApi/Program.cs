0c1c6ad728614438a19a7002ddc36878203bf9c9
George Nikolaidis <nove@novesoftware.net>
2025-05-05 13:22:21 +0300

    Configure Program.cs to work with Serilog

--- LedgerLite/src/LedgerLite.WebApi/Program.cs
+++ LedgerLite/src/LedgerLite.WebApi/Program.cs
@@ -1,14 +1,38 @@
 using FastEndpoints;
 using LedgerLite.WebApi;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+try
+{
+    Log.Information("Starting LedgerLite web API...");
+    
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddModules(builder.Configuration)
-    .AddApiInfrastructure();
+    builder.Services.AddSerilog((services, config) => config
+        .ReadFrom.Configuration(builder.Configuration));
 
-var app = builder.Build();
+    builder.Services
+        .AddModules(builder.Configuration)
+        .AddApiInfrastructure();
 
-app.MapOpenApi();
-app.UseFastEndpoints();
-app.Run();
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+    app.MapOpenApi();
+    app.UseFastEndpoints();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "LedgerLite ended abruptly.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
