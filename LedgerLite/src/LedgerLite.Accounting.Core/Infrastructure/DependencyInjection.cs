1ba3663a69d1585f2862da86447032b822af5f8f
George Nikolaidis <nove@novesoftware.net>
2025-05-05 13:21:08 +0300

    Add DI file for Accounting.Core infrastructure

---
+++ LedgerLite/src/LedgerLite.Accounting.Core/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using LedgerLite.Accounting.Core.Application;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LedgerLite.Accounting.Core.Infrastructure;
+
+internal static class DependencyInjection
+{
+    public static IServiceCollection AddAccountingInfrastructure(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IAccountingUnitOfWork, AccountingUnitOfWork>()
+            .AddScoped<IChartOfAccountsRepository, ChartOfAccountsRepository>()
+            .AddScoped<IAccountRepository, AccountRepository>()
+            .AddScoped<IJournalEntryRepository, JournalEntryRepository>()
+            .AddScoped<IJournalEntryLineRepository, JournalEntryLineRepository>();
+    } 
+}
