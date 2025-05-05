072cd854013d6a5f17aab0c54e0b6b8a07c46aee
George Nikolaidis <nove@novesoftware.net>
2025-05-05 13:20:51 +0300

    Add Result<T>-based enum conversions

---
+++ LedgerLite/src/LedgerLite.SharedKernel/Extensions/Enumeration.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+using Ardalis.SmartEnum;
+
+namespace LedgerLite.SharedKernel.Extensions;
+
+public static class Enumeration<TEnum> where TEnum : SmartEnum<TEnum>
+{
+    public static Result<TEnum> FromName(string name) =>
+        SmartEnum<TEnum>.TryFromName(name, ignoreCase: true, out var value)
+            ? Result.Success(value)
+            : Result.Invalid(new ValidationError($"'{name}' is not a valid {typeof(TEnum).Name}"));
+}
