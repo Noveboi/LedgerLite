c5ab96cc50c90d94225ddb6e399e9e0367fd68cf
George Nikolaidis <nove@novesoftware.net>
2025-05-06 21:04:57 +0300

    Add Microsoft.AspNetCore.Identity to `Users` module and configure custom UsersDbContext

---
+++ LedgerLite/src/LedgerLite.Users/Infrastructure/UsersDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LedgerLite.Users.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LedgerLite.Users.Infrastructure;
+
+internal sealed class UsersDbContext(DbContextOptions<UsersDbContext> options) : IdentityDbContext<User>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.HasDefaultSchema("Users");
+    }
+}
---
+++ LedgerLite/src/LedgerLite.Users/UsersDependencyInjection.cs
@@ -0,0 +1,47 @@
+﻿using LedgerLite.SharedKernel.Constants;
+using LedgerLite.SharedKernel.Extensions;
+using LedgerLite.Users.Domain;
+using LedgerLite.Users.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace LedgerLite.Users;
+
+public static class UsersDependencyInjection
+{
+    public static IServiceCollection AddLedgerLiteAuth(this IServiceCollection services)
+    {
+        Log.Information("Registering Authentication/Authorization.");
+        
+        services.AddAuthorization();
+        services.AddAuthentication(IdentityConstants.BearerScheme)
+            .AddCookie(IdentityConstants.ApplicationScheme)
+            .AddBearerToken(IdentityConstants.BearerScheme);
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddUsersModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        Log.Logger.RegisteringModule("Users");
+
+        services.AddDbContext<UsersDbContext>(options => options
+            .UseNpgsql(configuration.GetConnectionString(ConnectionStrings.CoreDatabase)));
+        
+        return services
+            .AddIdentityCore<User>()
+            .AddEntityFrameworkStores<UsersDbContext>()
+            .AddApiEndpoints()
+            .Services;
+    }
+
+    public static IEndpointConventionBuilder MapUserEndpoints(this IEndpointRouteBuilder app)
+    {
+        return app.MapIdentityApi<User>();
+    }
+}
