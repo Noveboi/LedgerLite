e0a812f98c190f18e4dd522f775bd3b35cb7e2d2
George Nikolaidis <nove@novesoftware.net>
2025-05-07 10:28:05 +0300

    Add helpful result assertion extensions

---
+++ LedgerLite/test/LedgerLite.Tests.Shared/ResultAssertionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+
+namespace LedgerLite.Tests.Shared;
+
+public static class ResultAssertionExtensions
+{
+    public static void ShouldHaveError(this Result result, ValidationError error) => result.ValidationErrors
+        .ShouldHaveSingleItem()
+        .ShouldBeEquivalentTo(error);
+
+    public static void ShouldBeInvalid(this Result result) => result.Status.ShouldBe(ResultStatus.Invalid);
+}
