6da70fb5b3cab4e762137b5e51c8fc1f26764d8c
George Nikolaidis <nove@novesoftware.net>
2025-05-07 12:25:20 +0300

    Add `Users` infrastructure DI

---
+++ LedgerLite/src/LedgerLite.Users/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using LedgerLite.Users.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LedgerLite.Users.Infrastructure;
+
+internal static class DependencyInjection
+{
+    public static IServiceCollection AddUsersInfrastructure(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IUserUnitOfWork, UserUnitOfWork>()
+            .AddScoped<IOrganizationRepository, OrganizationRepository>();
+    }
+}
