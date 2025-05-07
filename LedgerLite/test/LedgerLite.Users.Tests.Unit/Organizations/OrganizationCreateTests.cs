bfe90ab5a187e3a533046aecfd186adc37d80066
George Nikolaidis <nove@novesoftware.net>
2025-05-07 10:27:16 +0300

    Add `Organization` creation tests

---
+++ LedgerLite/test/LedgerLite.Users.Tests.Unit/Organizations/OrganizationCreateTests.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.Result;
+using LedgerLite.SharedKernel.Domain.Errors;
+using LedgerLite.Users.Domain.Organizations;
+
+namespace LedgerLite.Users.Tests.Unit.Organizations;
+
+public class OrganizationCreateTests
+{
+    [Fact]
+    public void Invalid_WhenNameIsEmptyOrWhitespace()
+    {
+        var result = Organization.Create("");
+        
+        result.Status.ShouldBe(ResultStatus.Invalid);
+        
+        result.ValidationErrors
+            .ShouldHaveSingleItem()
+            .ShouldBeEquivalentTo(CommonErrors.NameIsEmpty());
+    }
+
+    [Fact]
+    public void Create_WhenNameIsValid()
+    {
+        var result = Organization.Create("Test Org!");
+        result.Status.ShouldBe(ResultStatus.Ok);
+        result.Value.Name.ShouldBe("Test Org!");
+        result.Value.Members.ShouldBeEmpty();
+    }
+}
