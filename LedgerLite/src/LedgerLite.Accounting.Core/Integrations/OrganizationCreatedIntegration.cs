b484d84edf3d5c480b4e8c1a3ecca5a0a6a3df66
George Nikolaidis <nove@novesoftware.net>
2025-05-07 12:28:18 +0300

    Add an integration event handler in `Accounting` module for OrganizationCreated

---
+++ LedgerLite/src/LedgerLite.Accounting.Core/Integrations/OrganizationCreatedIntegration.cs
@@ -0,0 +1,14 @@
+﻿using LedgerLite.SharedKernel.Events;
+using LedgerLite.Users.Contracts.IntegrationEvents;
+using Serilog;
+
+namespace LedgerLite.Accounting.Core.Integrations;
+
+public sealed class OrganizationCreatedIntegration : IEventHandler<OrganizationCreatedIntegrationEvent>
+{
+    public ValueTask HandleAsync(OrganizationCreatedIntegrationEvent e, CancellationToken token)
+    {
+        Log.Information("Accounting module received integration event {@event}", e);
+        return ValueTask.CompletedTask;
+    }
+}
