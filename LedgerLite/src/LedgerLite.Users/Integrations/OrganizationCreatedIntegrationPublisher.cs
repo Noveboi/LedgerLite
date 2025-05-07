0d9bb747aed873e780cce7374ce6bb91ab1b9bcb
George Nikolaidis <nove@novesoftware.net>
2025-05-07 12:27:19 +0300

    Add a OrganizationCreatedIntegrationEvent publisher in `Users` module

---
+++ LedgerLite/src/LedgerLite.Users/Integrations/OrganizationCreatedIntegrationPublisher.cs
@@ -0,0 +1,14 @@
+﻿using LedgerLite.SharedKernel;
+using LedgerLite.SharedKernel.Events;
+using LedgerLite.Users.Contracts.IntegrationEvents;
+using LedgerLite.Users.Domain.Organizations.Events;
+
+namespace LedgerLite.Users.Integrations;
+
+
+internal sealed class OrganizationCreatedIntegrationPublisher(IPublisher publisher) : 
+    IntegrationEventPropagator<OrganizationCreatedEvent, OrganizationCreatedIntegrationEvent>(publisher)
+{
+    protected override OrganizationCreatedIntegrationEvent MapToIntegrationEvent(OrganizationCreatedEvent domainEvent) => 
+        new(domainEvent.Name);
+}
