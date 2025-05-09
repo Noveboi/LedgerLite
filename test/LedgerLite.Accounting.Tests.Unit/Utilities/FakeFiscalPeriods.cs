e6010217aefb018de546fb2aa6598b501e982ef3
George Nikolaidis <nove@novesoftware.net>
2025-05-09 13:27:42 +0300

    Add FakeFiscalPeriods for tests

---
+++ test/LedgerLite.Accounting.Tests.Unit/Utilities/FakeFiscalPeriods.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using LedgerLite.Accounting.Core.Domain.Periods;
+using LedgerLite.Tests.Shared;
+
+namespace LedgerLite.Accounting.Tests.Unit.Utilities;
+
+public static class FakeFiscalPeriods
+{
+    private static Faker<FiscalPeriod> Faker() => new PrivateFaker<FiscalPeriod>()
+        .UsePrivateConstructor()
+        .RuleFor(x => x.StartDate, f => f.Date.PastDateOnly())
+        .RuleFor(x => x.EndDate, (_, period) => period.StartDate.AddMonths(4))
+        .RuleFor(x => x.OrganizationId, _ => Guid.NewGuid());
+
+    public static FiscalPeriod Get() => Faker().Generate();
+}
