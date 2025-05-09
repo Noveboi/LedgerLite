b324796a29617bcaf4b5babdd90fdc3876815d38
George Nikolaidis <nove@novesoftware.net>
2025-05-09 13:26:53 +0300

    Add integration event consumer that creates a chart of accounts when an organization is created

---
+++ src/LedgerLite.Accounting.Core/Integrations/CreateChartOfAccountWhenOrganizationCreated.cs
@@ -0,0 +1,38 @@
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Core.Domain.Chart;
+using LedgerLite.Accounting.Core.Infrastructure;
+using LedgerLite.SharedKernel.Events;
+using LedgerLite.Users.Contracts.IntegrationEvents;
+using Serilog;
+
+namespace LedgerLite.Accounting.Core.Integrations;
+
+public sealed class CreateChartOfAccountWhenOrganizationCreated(IAccountingUnitOfWork unitOfWork) 
+    : IEventHandler<OrganizationCreatedIntegrationEvent>
+{
+    private readonly ILogger _log = Log.ForContext<CreateChartOfAccountWhenOrganizationCreated>();
+    
+    public async ValueTask HandleAsync(OrganizationCreatedIntegrationEvent e, CancellationToken token)
+    {
+        _log.Information("Creating new '{chartName}' because an organization was created.", nameof(ChartOfAccounts));
+
+        var result = await ChartOfAccounts.Create(organizationId: e.Id)
+            .Bind(chart =>
+            {
+                unitOfWork.ChartOfAccountsRepository.Add(chart);
+                return Result.Success(chart);
+            })
+            .BindAsync(chart => unitOfWork.SaveChangesAsync(token).MapAsync(() => chart));
+
+        if (!result.IsSuccess)
+        {
+            _log.Error("Could not create {chartName}. Error: {@error}", nameof(ChartOfAccounts), result);
+            return;
+        }
+        
+        _log.Information("Created {chartName} ({chartId})  for organization '{orgName}'", 
+            nameof(ChartOfAccounts), 
+            e.Name,
+            result.Value.Id);
+    }
+}
