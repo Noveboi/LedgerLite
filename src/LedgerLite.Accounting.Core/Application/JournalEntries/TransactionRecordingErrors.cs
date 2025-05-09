653dcd5aa8c9daeff2c9210cdc7cf2d17e5d7cc5
George Nikolaidis <nove@novesoftware.net>
2025-05-09 13:29:50 +0300

    Add TransactionRecordingErrors

---
+++ src/LedgerLite.Accounting.Core/Application/JournalEntries/TransactionRecordingErrors.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Result;
+
+namespace LedgerLite.Accounting.Core.Application.JournalEntries;
+
+public static class TransactionRecordingErrors
+{
+    public const string TransactionIdentifier = "TransactionRecording";
+
+    public static ValidationError FiscalPeriodNotFound(Guid periodId) => new(
+        identifier: TransactionIdentifier,
+        errorMessage: $"Fiscal period '{periodId}' does not exist.",
+        errorCode: "TRA-FP_NOT_FOUND",
+        severity: ValidationSeverity.Error);
+}
