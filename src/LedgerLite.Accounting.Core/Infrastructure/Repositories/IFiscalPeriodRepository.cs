0e9c0d595cac380725d75ac27f29752b6befe99f
George Nikolaidis <nove@novesoftware.net>
2025-05-09 13:27:52 +0300

    Add IFiscalPeriodRepository.cs

---
+++ src/LedgerLite.Accounting.Core/Infrastructure/Repositories/IFiscalPeriodRepository.cs
@@ -0,0 +1,10 @@
+﻿using LedgerLite.Accounting.Core.Domain.Periods;
+
+namespace LedgerLite.Accounting.Core.Infrastructure.Repositories;
+
+public interface IFiscalPeriodRepository
+{
+    void Add(FiscalPeriod period);
+
+    Task<FiscalPeriod?> GetByIdAsync(Guid id, CancellationToken token);
+}
