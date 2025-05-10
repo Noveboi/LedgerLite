a3f81ca9270b88195ea1c0c017acc42b8486d740
George Nikolaidis <nove@novesoftware.net>
2025-05-10 09:11:08 +0300

    Create unimplemented JournalEntry service

---
+++ src/LedgerLite.Accounting.Core/Application/JournalEntries/IJournalEntryService.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Core.Domain.JournalEntries;
+
+namespace LedgerLite.Accounting.Core.Application.JournalEntries;
+
+/// <summary>
+/// Handles simple operations regarding journal entries.
+/// For more actions regarding journal entries, see <see cref="ITransactionRecordingService"/>
+/// </summary>
+public interface IJournalEntryService
+{
+    Task<Result<JournalEntry>> GetByIdAsync(Guid id, CancellationToken token);
+}
---
+++ src/LedgerLite.Accounting.Core/Application/JournalEntries/JournalEntryService.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Core.Domain.JournalEntries;
+
+namespace LedgerLite.Accounting.Core.Application.JournalEntries;
+
+internal sealed class JournalEntryService : IJournalEntryService
+{
+    public Task<Result<JournalEntry>> GetByIdAsync(Guid id, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
