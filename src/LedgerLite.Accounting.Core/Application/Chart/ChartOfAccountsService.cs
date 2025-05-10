588f12e31d85109726773a5c3d2bc203cdc68c87
George Nikolaidis <nove@novesoftware.net>
2025-05-10 09:11:33 +0300

    Add ChartOfAccounts service

---
+++ src/LedgerLite.Accounting.Core/Application/Chart/ChartOfAccountsService.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Core.Domain.Chart;
+using LedgerLite.Accounting.Core.Infrastructure.Repositories;
+
+namespace LedgerLite.Accounting.Core.Application.Chart;
+
+public sealed class ChartOfAccountsService(IChartOfAccountsRepository repository) : IChartOfAccountsService
+{
+    public async Task<Result<ChartOfAccounts>> GetByOrganizationIdAsync(Guid? organizationId, CancellationToken token)
+    {
+        if (!organizationId.HasValue)
+        {
+            return Result.NotFound("User does not belong in an organization.");
+        }
+
+        if (await repository.GetByOrganizationIdAsync(organizationId.Value, token) is not { } chart)
+        {
+            return Result.NotFound($"Organization with ID '{organizationId.Value}' does not exist.");
+        }
+
+        return chart;
+    }
+}
---
+++ src/LedgerLite.Accounting.Core/Application/Chart/IChartOfAccountsService.cs
@@ -0,0 +1,9 @@
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Core.Domain.Chart;
+
+namespace LedgerLite.Accounting.Core.Application.Chart;
+
+public interface IChartOfAccountsService
+{
+    Task<Result<ChartOfAccounts>> GetByOrganizationIdAsync(Guid? organizationId, CancellationToken token);
+}
