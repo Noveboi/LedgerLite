87fe7eb167c9796eda365515e2455405bb1816d8
George Nikolaidis <nove@novesoftware.net>
2025-05-10 12:17:10 +0300

    Implement JournalEntryService

--- src/LedgerLite.Accounting.Core/Application/JournalEntries/JournalEntryService.cs
+++ src/LedgerLite.Accounting.Core/Application/JournalEntries/JournalEntryService.cs
@@ -1,12 +1,12 @@
 ﻿using Ardalis.Result;
 using LedgerLite.Accounting.Core.Domain.JournalEntries;
+using LedgerLite.Accounting.Core.Infrastructure.Repositories;
 
 namespace LedgerLite.Accounting.Core.Application.JournalEntries;
 
-internal sealed class JournalEntryService : IJournalEntryService
+internal sealed class JournalEntryService(IJournalEntryRepository repository) : IJournalEntryService
 {
-    public Task<Result<JournalEntry>> GetByIdAsync(Guid id, CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<Result<JournalEntry>> GetByIdAsync(Guid id, CancellationToken token) =>
+        await repository.GetByIdAsync(id, token) ?? 
+        Result<JournalEntry>.NotFound($"Couldn't find Journal Entry with ID '{id}'");
 }
