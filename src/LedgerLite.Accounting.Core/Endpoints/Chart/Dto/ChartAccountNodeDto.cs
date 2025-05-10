7673fda4675d8637ef88100ede73306520bff9b1
George Nikolaidis <nove@novesoftware.net>
2025-05-10 12:13:50 +0300

    Rename AccountNodeDto to ChartAccountNodeDto

--- src/LedgerLite.Accounting.Core/Endpoints/Chart/Dto/ChartAccountNodeDto.cs
+++ src/LedgerLite.Accounting.Core/Endpoints/Chart/Dto/ChartAccountNodeDto.cs
@@ -3,11 +3,16 @@ using LedgerLite.Accounting.Core.Endpoints.Accounts.Dto;
 
 namespace LedgerLite.Accounting.Core.Endpoints.Chart.Dto;
 
-public sealed record AccountNodeDto(
+/// <summary>
+/// DTO for when an <see cref="AccountNode"/> is part of a <see cref="ChartOfAccounts"/> DTO.
+/// </summary>
+/// <param name="Account"></param>
+/// <param name="ParentAccountId"></param>
+public sealed record ChartAccountNodeDto(
     SlimAccountDto Account,
     Guid? ParentAccountId)
 {
-    public static AccountNodeDto FromEntity(AccountNode node) => new(
+    public static ChartAccountNodeDto FromEntity(AccountNode node) => new(
         Account: SlimAccountDto.FromEntity(node.Account),
         ParentAccountId: node.ParentId);
 }
--- src/LedgerLite.Accounting.Core/Endpoints/Chart/Dto/ChartOfAccountsDto.cs
+++ src/LedgerLite.Accounting.Core/Endpoints/Chart/Dto/ChartOfAccountsDto.cs
@@ -5,10 +5,10 @@ namespace LedgerLite.Accounting.Core.Endpoints.Chart.Dto;
 public sealed record ChartOfAccountsDto(
     Guid Id,
     Guid OrganizationId,
-    IEnumerable<AccountNodeDto> Accounts)
+    IEnumerable<ChartAccountNodeDto> Accounts)
 {
     public static ChartOfAccountsDto FromEntity(ChartOfAccounts chart) => new(
         Id: chart.Id,
         OrganizationId: chart.OrganizationId,
-        Accounts: chart.Nodes.Select(AccountNodeDto.FromEntity));
+        Accounts: chart.Nodes.Select(ChartAccountNodeDto.FromEntity));
 }
