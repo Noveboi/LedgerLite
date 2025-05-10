a2dfe95e8437e1a0989273ab0b96b14386b11cbc
George Nikolaidis <nove@novesoftware.net>
2025-05-10 12:17:20 +0300

    Add GetDetailedAccountUseCase

---
+++ src/LedgerLite.Accounting.Core/Application/UseCases/GetDetailedAccountUseCase.cs
@@ -0,0 +1,36 @@
+﻿using Ardalis.Result;
+using LedgerLite.Accounting.Core.Application.Chart;
+using LedgerLite.Accounting.Core.Domain.Accounts;
+using LedgerLite.Accounting.Core.Domain.Chart;
+using LedgerLite.Accounting.Core.Infrastructure.Repositories;
+using LedgerLite.SharedKernel.Domain.Errors;
+using LedgerLite.SharedKernel.UseCases;
+
+namespace LedgerLite.Accounting.Core.Application.UseCases;
+
+internal sealed record GetDetailedAccountRequest(Guid UserId, Guid AccountId);
+internal sealed class GetDetailedAccountUseCase(
+    IChartOfAccountsService chartService,
+    IJournalEntryLineRepository lineRepository)
+    : IApplicationUseCase<GetDetailedAccountRequest, AccountWithDetails>
+{
+    public async Task<Result<AccountWithDetails>> HandleAsync(GetDetailedAccountRequest request, CancellationToken token)
+    {
+        var chartResult = await chartService.GetByUserIdAsync(request.UserId, token);
+        if (!chartResult.IsSuccess)
+        {
+            return chartResult.Map();
+        }
+
+        var chart = chartResult.Value;
+        if (chart.Nodes.FirstOrDefault(node => node.Account.Id == request.AccountId) is not { } accountNode)
+        {
+            return Result.NotFound(CommonErrors.NotFound<Account>(request.AccountId));
+        }
+
+        var lines = await lineRepository.GetLinesForAccountAsync(accountNode.Account);
+        return new AccountWithDetails(
+            Node: accountNode,
+            Lines: lines);
+    }
+}
