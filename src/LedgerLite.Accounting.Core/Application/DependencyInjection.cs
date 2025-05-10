2b3ffd35fb90a25304c6fbe8bfc1736958ac2120
George Nikolaidis <nove@novesoftware.net>
2025-05-10 09:11:46 +0300

    Add new services to DI

--- src/LedgerLite.Accounting.Core/Application/DependencyInjection.cs
+++ src/LedgerLite.Accounting.Core/Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using LedgerLite.Accounting.Core.Application.Accounts;
+using LedgerLite.Accounting.Core.Application.Chart;
 using LedgerLite.Accounting.Core.Application.JournalEntries;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,7 +10,9 @@ internal static class DependencyInjection
     public static IServiceCollection AddAccountingApplication(this IServiceCollection services)
     {
         return services
+            .AddScoped<IChartOfAccountsService, ChartOfAccountsService>()
             .AddScoped<IAccountService, AccountService>()
+            .AddScoped<IJournalEntryService, JournalEntryService>()
             .AddScoped<ITransactionRecordingService, TransactionRecordingService>();
     }
 }
