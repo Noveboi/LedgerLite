1a18d2f809ec7cb3d5523b9b6c0685032da39704
George Nikolaidis <nove@novesoftware.net>
2025-05-10 12:16:18 +0300

    Use new AssemblyScanner API for event pub/sub registration

--- src/LedgerLite.SharedKernel/Events/DependencyInjection.cs
+++ src/LedgerLite.SharedKernel/Events/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using LedgerLite.SharedKernel.Internal;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
@@ -8,38 +9,8 @@ internal static class DependencyInjection
 {
     public static IServiceCollection AddEventInfrastructure(this IServiceCollection services)
     {
-        var time = Stopwatch.GetTimestamp();
-        var handlerInterfaceType = typeof(IEventHandler<>);
-
-        var assemblyScanCount = 0;
-        var handlerTypes = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(assembly =>
-            {
-                assemblyScanCount++;
-                Log.Debug("Searching for event handlers in {assemblyName}", assembly.FullName);
-                return assembly
-                    .GetTypes()
-                    .Where(t => t is { IsAbstract: false, IsClass: true } && t
-                        .GetInterfaces()
-                        .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerInterfaceType)
-                    );
-            })
-            .ToList();
-        
-        Log.Information("Registered {handlerCount} event handlers from {assemblyCount} assemblies in {ms} milliseconds", 
-            handlerTypes.Count, assemblyScanCount, Stopwatch.GetElapsedTime(time).TotalMilliseconds.ToString("N0"));
-
-        foreach (var handlerImplementationType in handlerTypes)
-        {
-            var interfaces = handlerImplementationType.GetInterfaces()
-                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerInterfaceType);
-
-            foreach (var @interface in interfaces)
-            {
-                services.AddScoped(@interface, handlerImplementationType);
-            }
-        }
-        
+        Log.Information("Configuring {name}", "Event Pub/Sub Infrastructure");
+        AssemblyScanner.RegisterServices(services, o => o.Implementing(typeof(IEventHandler<>)));
         return services
             .AddScoped<IPublisher, Publisher>()
             .AddScoped<IEventPublisher, SequentialEventPublisher>();
