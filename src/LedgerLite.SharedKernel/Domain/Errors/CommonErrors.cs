cff0da18dc7bc79d6273e0b87c2b26ba79e23bcb
George Nikolaidis <nove@novesoftware.net>
2025-05-15 16:51:41 +0300

    Remove Entity generic constraint in CommonErrors

--- src/LedgerLite.SharedKernel/Domain/Errors/CommonErrors.cs
+++ src/LedgerLite.SharedKernel/Domain/Errors/CommonErrors.cs
@@ -13,7 +13,7 @@ public static class CommonErrors
         errorCode: "NAME_EMPTY",
         severity: ValidationSeverity.Error);
 
-    public static string NotFound<T>(Guid id) where T : Entity
+    public static string NotFound<T>(Guid id) where T : class
     {
         var name = typeof(T).Name;
         return $"{name.Humanize(LetterCasing.Title)} with ID '{id}' does not exist";
