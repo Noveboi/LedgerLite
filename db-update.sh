ed95f349c30143ed6c89ca7fc048c261434f9ed5
Nove <cptgiorgos03@gmail.com>
2025-05-20 19:55:25 +0300

    Improve UX for db-update script by automatically launching Compose and updating/installing the dotnet-ef tool

--- db-update.sh
+++ db-update.sh
@@ -1,3 +1,6 @@
+docker compose up -d --build || exit 1
+dotnet tool install dotnet-ef --create-manifest-if-needed
+
 dotnet build ./src/LedgerLite.WebApi || exit 1 
 dotnet ef database update --startup-project ./src/LedgerLite.WebApi --project ./src/LedgerLite.Accounting.Core --context AccountingDbContext --no-build
 dotnet ef database update --startup-project ./src/LedgerLite.WebApi --project ./src/LedgerLite.Users --context UsersDbContext --no-build
