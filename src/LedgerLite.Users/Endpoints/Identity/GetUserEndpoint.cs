d782efef7a8a5ec1304dc6e4ed698fdd56e5cb29
George Nikolaidis <nove@novesoftware.net>
2025-05-20 22:15:43 +0300

    Reduce code duplication and refactor endpoints to use new DTO

--- src/LedgerLite.Users/Endpoints/Identity/GetUserEndpoint.cs
+++ src/LedgerLite.Users/Endpoints/Identity/GetUserEndpoint.cs
@@ -1,15 +1,14 @@
+using Ardalis.Result.AspNetCore;
 using FastEndpoints;
 using LedgerLite.SharedKernel.Identity;
+using LedgerLite.Users.Contracts;
 using LedgerLite.Users.Contracts.Models;
-using LedgerLite.Users.Domain;
-using LedgerLite.Users.Integrations.Conversions;
-using Microsoft.AspNetCore.Identity;
 
 namespace LedgerLite.Users.Endpoints.Identity;
 
 internal sealed record GetUserRequest([property: FromClaim(LedgerClaims.UserId)] Guid UserId);
 
-internal sealed class GetUserEndpoint(UserManager<User> userManager) : Endpoint<GetUserRequest, UserDto>
+internal sealed class GetUserEndpoint(IUserRequests requests) : Endpoint<GetUserRequest, UserDto>
 {
     public override void Configure()
     {
@@ -19,12 +18,7 @@ internal sealed class GetUserEndpoint(UserManager<User> userManager) : Endpoint<
 
     public override async Task HandleAsync(GetUserRequest req, CancellationToken ct)
     {
-        if (await userManager.FindByIdAsync(req.UserId.ToString()) is not { } user)
-        {
-            await SendUnauthorizedAsync(ct);
-            return;
-        }
-
-        await SendAsync(user.ToDto(), cancellation: ct);
+        var userResult = await requests.GetUserByIdAsync(req.UserId, ct);
+        await SendResultAsync(userResult.ToMinimalApiResult());
     }
 }
--- src/LedgerLite.Users/Integrations/Conversions/UsersToDtoConversions.cs
+++ src/LedgerLite.Users/Integrations/Conversions/UsersToDtoConversions.cs
@@ -1,19 +1,26 @@
 ﻿using LedgerLite.Users.Contracts.Models;
 using LedgerLite.Users.Domain;
+using LedgerLite.Users.Domain.Organizations;
 
 namespace LedgerLite.Users.Integrations.Conversions;
 
 internal static class UsersToDtoConversions
 {
-    public static UserDto ToDto(this User user) => new(
-        Id: user.Id,
-        OrganizationId: user.OrganizationMember?.OrganizationId,
-        Username: user.UserName ?? "",
-        Email: user.Email ?? "",
-        FullName: (user.FirstName, user.LastName) switch
-        {
-            (not null, not null) => $"{user.FirstName} {user.LastName}",
-            (null, not null) => user.LastName,
-            _ => ""
-        });
+    public static UserDto ToDto(this User user, Organization? organization)
+    {
+        if (user.OrganizationMember?.OrganizationId is { } id && organization?.Id != id)
+            throw new ArgumentException("Organization does not match ID of user's organization", nameof(organization));
+                
+        return new UserDto(
+            Id: user.Id,
+            Organization: organization?.ToDto(),
+            Username: user.UserName ?? "",
+            Email: user.Email ?? "",
+            FullName: (user.FirstName, user.LastName) switch
+            {
+                (not null, not null) => $"{user.FirstName} {user.LastName}",
+                (null, not null) => user.LastName,
+                _ => ""
+            });
+    }
 }
