6e5306c59d1063882b03ca60b3eb563b21e0bd7b
Nove <cptgiorgos03@gmail.com>
2025-05-29 14:47:49 +0300

    Make TransactionType a CLR enum

--- src/LedgerLite.Accounting.Core/Domain/TransactionType.cs
+++ src/LedgerLite.Accounting.Core/Domain/TransactionType.cs
@@ -1,18 +1,15 @@
-﻿using Ardalis.SmartEnum;
+﻿namespace LedgerLite.Accounting.Core.Domain;
 
-namespace LedgerLite.Accounting.Core.Domain;
-
-public sealed class TransactionType : SmartEnum<TransactionType>
+public enum TransactionType
 {
-    private TransactionType() : this("", 0) { }
-    private TransactionType(string name, int value) : base(name, value) { }
 
     /// <summary>
     /// Increases the value of liability, equity and revenue.
     /// </summary>
-    public static readonly TransactionType Credit = new(nameof(Credit), 1); 
+    Credit = 1,
+    
     /// <summary>
     /// Increases the value of assets and expenses.
     /// </summary>
-    public static readonly TransactionType Debit = new(nameof(Debit), 2);
+    Debit = 2
 }
