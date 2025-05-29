665c74047aab41ccfeb2352720f9aaf870aab71e
Nove <cptgiorgos03@gmail.com>
2025-05-29 20:33:51 +0300

    Add `Get Organizations` endpoint

---
+++ src/LedgerLite.Users/Endpoints/Organizations/GetOrganizationsEndpoint.cs
@@ -0,0 +1,22 @@
+using FastEndpoints;
+using LedgerLite.Users.Contracts.Models;
+using LedgerLite.Users.Infrastructure.Repositories;
+using LedgerLite.Users.Integrations.Conversions;
+
+namespace LedgerLite.Users.Endpoints.Organizations;
+
+internal sealed class GetOrganizationsEndpoint(IOrganizationRepository organizationRepository) 
+    : EndpointWithoutRequest<IEnumerable<OrganizationDto>>
+{
+    public override void Configure()
+    {
+        Get("");
+        Group<OrganizationEndpointGroup>();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var organizations = await organizationRepository.GetAllAsync(ct);
+        await SendAsync(organizations.Select(x => x.ToDto()), cancellation: ct);
+    }
+}
