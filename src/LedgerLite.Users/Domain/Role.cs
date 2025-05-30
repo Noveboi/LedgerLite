5db589833078d306a2b9fa05fc01d9b950ea80ca
George Nikolaidis <nove@novesoftware.net>
2025-05-30 19:48:51 +0300

    Add constructor to Role

--- src/LedgerLite.Users/Domain/Role.cs
+++ src/LedgerLite.Users/Domain/Role.cs
@@ -4,6 +4,14 @@ namespace LedgerLite.Users.Domain;
 
 public sealed class Role : IdentityRole<Guid>
 {
+    private Role() { }
+
+    public Role(string name, string description = "")
+    {
+        Name = name;
+        Description = description;
+    }
+    
     public IReadOnlyCollection<UserRole> UserRoles { get; init; } = null!;
     public string? Description { get; init; }
 }
