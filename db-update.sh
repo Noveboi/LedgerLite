235e1e17a8db3a5972779ab32433fabe2d7022ed
George Nikolaidis <nove@novesoftware.net>
2025-05-30 19:16:16 +0300

    Polish db-update.sh

--- db-update.sh
+++ db-update.sh
@@ -1,6 +1,6 @@
 docker compose up -d --build || exit 1
-dotnet tool install dotnet-ef --create-manifest-if-needed
+dotnet tool install dotnet-ef --create-manifest-if-needed || exit 1
 
 dotnet build ./src/LedgerLite.WebApi || exit 1 
-dotnet ef database update --startup-project ./src/LedgerLite.WebApi --project ./src/LedgerLite.Accounting.Core --context AccountingDbContext --no-build
+dotnet ef database update --startup-project ./src/LedgerLite.WebApi --project ./src/LedgerLite.Accounting.Core --context AccountingDbContext --no-build || exit 1
 dotnet ef database update --startup-project ./src/LedgerLite.WebApi --project ./src/LedgerLite.Users --context UsersDbContext --no-build
