2f34fe0b4baa4404b246cef96b9e36039cdea2a8
George Nikolaidis <nove@novesoftware.net>
2025-05-30 19:48:42 +0300

    Add Role DTO

---
+++ src/LedgerLite.Users.Contracts/Models/RoleDto.cs
@@ -0,0 +1,3 @@
+﻿namespace LedgerLite.Users.Contracts.Models;
+
+public sealed record RoleDto(Guid Id, string Name, string Description);
---
+++ src/LedgerLite.Users/Integrations/Conversions/RolesToDtoConversions.cs
@@ -0,0 +1,12 @@
+﻿using LedgerLite.Users.Contracts.Models;
+using LedgerLite.Users.Domain;
+
+namespace LedgerLite.Users.Integrations.Conversions;
+
+internal static class RolesToDtoConversions
+{
+    public static RoleDto ToDto(this Role role) => new(
+        Id: role.Id, 
+        Name: role.Name ?? "", 
+        Description: role.Description ?? "");
+}
