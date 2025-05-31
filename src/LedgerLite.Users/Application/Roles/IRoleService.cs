eabc1baf0e974da4f5cd1db274d9817e58ee811a
George Nikolaidis <nove@novesoftware.net>
2025-05-31 19:41:21 +0300

    Add role service with interface for easy role retrieval and testing

---
+++ src/LedgerLite.Users/Application/Roles/IRoleService.cs
@@ -0,0 +1,9 @@
+﻿using Ardalis.Result;
+using LedgerLite.Users.Domain;
+
+namespace LedgerLite.Users.Application.Roles;
+
+public interface IRoleService
+{
+    Task<Result<Role>> GetByNameAsync(string name, CancellationToken ct);
+}
---
+++ src/LedgerLite.Users/Application/Roles/RoleService.cs
@@ -0,0 +1,20 @@
+﻿using Ardalis.Result;
+using LedgerLite.Users.Domain;
+using Microsoft.AspNetCore.Identity;
+
+namespace LedgerLite.Users.Application.Roles;
+
+internal sealed class RoleService(IRoleStore<Role> store, RoleManager<Role> manager) : IRoleService
+{
+    public async Task<Result<Role>> GetByNameAsync(string name, CancellationToken ct)
+    {
+        var normalizedName = manager.NormalizeKey(name);
+
+        if (await store.FindByNameAsync(normalizedName, ct) is not { } role)
+        {
+            return Result.NotFound($"Role '{normalizedName}' does not exist.");
+        }
+
+        return role;
+    }
+}
