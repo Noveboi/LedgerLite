5422f6ce39d18c878c2b39a179e0f134001b2f64
George Nikolaidis <nove@novesoftware.net>
2025-05-31 20:35:33 +0300

    Add Includes to Organization query

--- src/LedgerLite.Users/Infrastructure/Repositories/OrganizationRepository.cs
+++ src/LedgerLite.Users/Infrastructure/Repositories/OrganizationRepository.cs
@@ -9,7 +9,12 @@ internal sealed class OrganizationRepository(UsersDbContext context) : IOrganiza
         context.Organizations.AnyAsync(x => x.Name == name, cancellationToken: token);
 
     public Task<Organization?> GetByIdAsync(Guid id, CancellationToken token) => 
-        context.Organizations.FirstOrDefaultAsync(x => x.Id == id, cancellationToken: token);
+        context.Organizations
+            .AsSplitQuery()
+            .Include(x => x.Members)
+            .ThenInclude(x => x.Roles)
+            .ThenInclude(x => x.Role)
+            .FirstOrDefaultAsync(x => x.Id == id, cancellationToken: token);
 
     public async Task<IReadOnlyList<Organization>> GetAllAsync(CancellationToken token) =>
         await context.Organizations.ToListAsync(cancellationToken: token);
