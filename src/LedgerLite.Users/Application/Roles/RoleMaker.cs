bd02d984f372ba9626487261ff9bc614eaf5890f
George Nikolaidis <nove@novesoftware.net>
2025-05-31 19:41:41 +0300

    Enhance RoleMaker by adding existence checks and validation

---
+++ src/LedgerLite.Users/Application/Roles/RoleMaker.cs
@@ -0,0 +1,54 @@
+﻿using Ardalis.Result;
+using LedgerLite.Users.Domain;
+using Microsoft.AspNetCore.Identity;
+using Serilog;
+
+namespace LedgerLite.Users.Application.Roles;
+
+internal sealed class RoleMaker(RoleManager<Role> roleManager)
+{
+    private readonly ILogger _log = Log.ForContext<RoleMaker>();
+    private readonly Dictionary<string, bool> _existingRoles = [];
+    
+    public async Task<Result> CreateApplicationRolesAsync(CancellationToken token)
+    {
+        _log.Information("Ensuring essential application roles exist.");
+
+        await GetIfRoleExistsAsync(CommonRoles.Owner);
+        await GetIfRoleExistsAsync(CommonRoles.Admin);
+        await GetIfRoleExistsAsync(CommonRoles.Member);
+        await GetIfRoleExistsAsync(CommonRoles.Viewer);
+        
+        var owner = new Role(CommonRoles.Owner, "The owner of the organization");
+        var admin = new Role(CommonRoles.Admin, "Executive rights in the organization");
+        var member = new Role(CommonRoles.Member, "Standard organization member");
+        var viewer = new Role(CommonRoles.Viewer, "Member with read-only access to the organization");
+
+        return await CreateRoleAsync(owner)
+            .BindAsync(_ => CreateRoleAsync(admin))
+            .BindAsync(_ => CreateRoleAsync(member))
+            .BindAsync(_ => CreateRoleAsync(viewer));
+    }
+
+    private async Task GetIfRoleExistsAsync(string name)
+    {
+        var exists = await roleManager.RoleExistsAsync(name);
+        _existingRoles[name] = exists;
+    }
+    
+    private async Task<Result> CreateRoleAsync(Role role)
+    {
+        if (string.IsNullOrWhiteSpace(role.Name) || _existingRoles[role.Name])
+        {
+            return Result.NoContent();
+        }
+        
+        _log.Information("Adding role '{name}'", role.Name);
+        var result = await roleManager.CreateAsync(role);
+        return result.Succeeded
+            ? Result.Success()
+            : Result.Invalid(result.Errors.Select(x => new ValidationError(
+                identifier: x.Code, 
+                errorMessage: x.Description)));
+    }
+}
